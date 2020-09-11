2d41b2afa7b386ca85b840e089d85bf27909716d
hacklun <hacklun@mail.ru>
2020-09-11 11:46:44 +0300

    start control of snake

--- app.js
+++ app.js
@@ -12,4 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let speed = 0.9;
     let intervalTime = 0;
     let interval = 0;
+
+    //assign functions to keycodes
+    function control(e) {
+        squares[currentIndex].classList.remove('snake'); // we are removing the class of snake from ALL the squares
+
+        if (e.keyCode === 39) {
+            direction = 1; // if we press the right arrow on our keyboard, the snake will go right one
+        } else if (e.keyCode === 38) {
+            direction = -width; // if we press the up arrow, the snake will go back ten divs, appearing to go up
+        }
+    }
 })
