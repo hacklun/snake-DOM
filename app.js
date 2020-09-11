f38f40fc3cb8e2a0b2e0fd7d83e0efd9e310af23
hacklun <hacklun@mail.ru>
2020-09-11 11:20:46 +0300

    add start parameteres

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentIndex = 0; //so first div in our grid
     let appleIndex = 0;
     let currentSnake = [2, 1, 0]; // so the div in our grid being 2 (or the HEAD), and 0 being the end (TAIL, with all 1's being the body from new on)
+    let direction = 1;
+    let score = 0;
+    let speed = 0.9;
+    let intervalTime = 0;
+    let interval = 0;
 })
